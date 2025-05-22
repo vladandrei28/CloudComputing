cdc60a4817c5863b768c34d49ef62bf10c8df609
unknown <vladandrei311@yahoo.com>
2025-05-22 21:20:18 +0300

    adaugare de carduri

---
+++ utils/functions.js
@@ -0,0 +1,8 @@
+// utils/functions.js
+
+import {connectToDatabase,} from '@/lib/mongodb.js';
+
+export const getCollection = async collectionName => {
+	const {database,} = await connectToDatabase();
+	return database.collection(collectionName);
+};
